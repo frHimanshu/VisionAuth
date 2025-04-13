699e145aaa01600b8db1651b4f005ef541ae0816
Himanshu A Garode <himanshu2006garode@gmail.com>
2025-04-14 02:06:53 +0530

    Update Main.php

--- WelcomePlugin/src/WelcomePlugin/Main.php
+++ WelcomePlugin/src/WelcomePlugin/Main.php
@@ -3,14 +3,17 @@
 namespace WelcomePlugin;
 
 use pocketmine\plugin\PluginBase;
-use pocketmine\command\CommandSender;
 use pocketmine\command\Command;
+use pocketmine\command\CommandSender;
 use pocketmine\player\Player;
+use pocketmine\event\Listener;
+use pocketmine\event\player\PlayerJoinEvent;
 
-class Main extends PluginBase {
+class Main extends PluginBase implements Listener {
 
     public function onEnable(): void {
         $this->getLogger()->info("§a[WelcomePlugin] Plugin has been enabled!");
+        $this->getServer()->getPluginManager()->registerEvents($this, $this);
     }
 
     public function onDisable(): void {
@@ -20,12 +23,17 @@ class Main extends PluginBase {
     public function onCommand(CommandSender $sender, Command $command, string $label, array $args): bool {
         if (strtolower($command->getName()) === "welcome") {
             if ($sender instanceof Player) {
-                $sender->sendMessage("§bWelcome to the server, " . $sender->getName() . "!");
+                $sender->sendMessage("§bWelcome to the server, §a" . $sender->getName() . "§b!");
             } else {
-                $sender->sendMessage("§eThis command can only be run by a player.");
+                $sender->sendMessage("§eThis command can only be used by players.");
             }
             return true;
         }
         return false;
+    }
+
+    public function onJoin(PlayerJoinEvent $event): void {
+        $player = $event->getPlayer();
+        $player->sendMessage("§6Welcome, §e" . $player->getName() . "§6! Enjoy your stay! ✨");
     }
 }
